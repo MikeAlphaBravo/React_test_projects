76f4c9305327f2f1d5b7ad5cd33c72083a3095f5
Michael Brooks <mikeyabrooks@gmail.com>
2018-01-20 12:27:21 -0800

    bundled and refactored to fix syntax comma error

--- react-help-queue/src/index.jsx
+++ react-help-queue/src/index.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Help Queue</h1>,
-    <h2>{new Date().toLocaleTimeString()}</h2>,
-    <h3>3a</h3>,
-    <h3>Michael and Margaret</h3>,
-    <p><em>Firebase will not save record!</em></p>,
-  </div>
+    <h1>Help Queue</h1>
+    <h2>{new Date().toLocaleTimeString()}</h2>
+    <h3>3a</h3>
+    <h3>Michael and Margaret</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
   document.getElementById('react-app-root')
 );
