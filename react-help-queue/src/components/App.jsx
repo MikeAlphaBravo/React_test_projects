20fefe7be0011df64ad1784b914a05ffdcc4fc21
Michael Brooks <mikeyabrooks@gmail.com>
2018-01-20 13:07:49 -0800

    create TicketList component

--- react-help-queue/src/components/App.jsx
+++ react-help-queue/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import TicketList from "./TicketList";
 
 function App(){
   return (
     <div>
       <Header/>
       <h3>{new Date().toLocaleTimeString()}</h3>
+      <TicketList/>
       <h3>3a</h3>
       <h3>Thato and Haley</h3>
       <p><em>Firebase entries not saving!</em></p>
